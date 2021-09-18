ea969b8cda6937dbbdad86d35d7dec6f4e93aedc
mkia <mr.kiani@phinix.ir>
2021-09-18 17:06:53 +0430

    test multiple gauge metric

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const client = require('prom-client');
 const url = require('url') ;
+let client = require('prom-client');
 axios.defaults.timeout = parseInt(process.env.AXIOS_TIMEOUT);
 // httpProxy
 const HttpsProxyAgent = require("https-proxy-agent");
@@ -20,15 +20,14 @@ const querySchema = Joi.object({
     proxyURL: Joi.string().required()
 });
 // Gauges
-httpProxyUpGauge = new client.Gauge({ name: 'http_proxy_up', help: 'status of http proxy server'});
-httpProxyResponseTimeGauge = new client.Gauge({ name: 'http_proxy_response_time', help: 'response time of http proxy server'});
-httpProxyLastUpdateGauge = new client.Gauge({ name: 'http_proxy_last_update_seconds', help: 'http_proxy_last_update_seconds'});
-socksProxyUpGauge = new client.Gauge({ name: 'socks_proxy_up', help: 'status of socks proxy server'});
-socksProxyResponseTimeGauge = new client.Gauge({ name: 'socks_proxy_response_time', help: 'response time of socks proxy server'});
-socksProxyLastUpdateGauge = new client.Gauge({ name: 'socks_proxy_last_update_seconds', help: 'socks_proxy_last_update_seconds'});
+let probeSuccessGuage = new client.Gauge({ name: 'proxy_probe_kia', help: 'status of proxy server'});
+let probeDurationGuage = new client.Gauge({ name: 'proxy_probe_ducration_seconds', help: 'response time of proxy server'});
 
 
-async function updateHttpProxyMetrics(target,proxyHost,proxyPort,auth) {
+
+
+
+async function scrapeWithHttpProxy(target,proxyHost,proxyPort,auth) {
     let httpsAgent='';
     try {
         if(auth === null){
@@ -40,24 +39,19 @@ async function updateHttpProxyMetrics(target,proxyHost,proxyPort,auth) {
         console.log('starting sending request through proxy');
         start = new Date();
         const proxyRes = await axios.get(target, {httpsAgent});
-        var end = new Date() - start;
-
-        httpProxyUpGauge.set(1);
-        httpProxyLastUpdateGauge.set(Math.floor(Date.now() / 1000));
-        httpProxyResponseTimeGauge.set(end);
+        var duration = new Date() - start;
         console.log('finished sending request through http proxy');
         console.log('///////////////////////////////////////////');
-
+        return {'proxy_probe_success': 1, 'proxy_probe_duration_seconds': duration};
     } catch(e) {
         console.log(e);
         console.log('error on http proxy connection');
-        httpProxyUpGauge.set(0);
+        return {'proxy_probe_success': 0, 'proxy_probe_duration_seconds': 0};
     }
 }
 
 
-
-async function updateSocksProxyMetrics(target,proxyHost,proxyPort) {
+async function scrapeWithSocksProxy(target,proxyHost,proxyPort) {
     try {
         var proxy = `socks5://${proxyHost}:${proxyPort}`;
         var agent = new SocksProxyAgent(proxy);
@@ -72,24 +66,21 @@ async function updateSocksProxyMetrics(target,proxyHost,proxyPort) {
         console.log('starting sending request through socks proxy');
         var start = new Date();
         const socksProxyRes = await rp(options);
-        var end = new Date() - start;
+        var duration = new Date() - start;
         
-        socksProxyUpGauge.set(1);
-        socksProxyLastUpdateGauge.set(Math.floor(Date.now() / 1000));
-        socksProxyResponseTimeGauge.set(end);
         console.log('finished sending request through socks proxy');
         console.log('///////////////////////////////////////////');
-
+        return {'proxy_probe_success': 1, 'proxy_probe_duration_seconds': duration};
     } catch(e) {
         console.log(e);
         console.log('error on socks proxy connection');
-        socksProxyUpGauge.set(0);
+        return {'proxy_probe_success': 0, 'proxy_probe_duration_seconds': 0};
     }
 }
 
-
-
-
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 //app
 app.get('/probe', validator.query(querySchema), async (req, res) => {
@@ -101,16 +92,25 @@ app.get('/probe', validator.query(querySchema), async (req, res) => {
     let queryProxyPort = q.port;
     let queryProxyAuth = q.auth;
     
-
+    
     if(q.protocol.split(':')[0] == 'http'){
-        await updateHttpProxyMetrics(queryTarget,queryProxyHost,queryProxyPort,queryProxyAuth);
+        const res = await scrapeWithHttpProxy(queryTarget,queryProxyHost,queryProxyPort,queryProxyAuth);
+        probeSuccessGuage.set(res.proxy_probe_success);
+        probeDurationGuage.set(res.proxy_probe_duration_seconds);
+        // console.log(res);
     }else if(q.protocol.split(':')[0] == 'socks'){
-        await updateSocksProxyMetrics(queryTarget,queryProxyHost,queryProxyPort);
+        const res = await scrapeWithSocksProxy(queryTarget,queryProxyHost,queryProxyPort);
+        probeSuccessGuage.set(res.proxy_probe_success);
+        probeDurationGuage.set(res.proxy_probe_duration_seconds);
+        console.log(res);
+        await timeout(10000);
+        
     }else{
         return res.status(400).send('invalde proxy protocol found in proxyURL');
     }
 
     metrics = await client.register.metrics();
+    // client.register.clear();
     return res.status(200).send(metrics);
 });
 
